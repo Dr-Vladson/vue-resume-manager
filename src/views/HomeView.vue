b56080e9863d57788ea6175dad95e21a085df3db
Влад Любашенко <whatislaw007@outlook.com>
2024-01-14 22:03:40 +0200

    add Home-view styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,17 @@
 
 <template>
   <main>
-    <RouterLink to="/watch-resume">Watch resume</RouterLink>
-    <RouterLink to="/create-resume">Create resume</RouterLink>
+    <div class="home-container">
+
+    </div>
   </main>
 </template>
+<style scoped>
+.home-container {
+  display: flex;
+  gap: 20px;
+  margin: 0 auto;
+  padding: 0 30px;
+  max-width: 1200px;
+}
+</style>
