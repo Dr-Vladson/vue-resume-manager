412ecbd1ef730efd6eb91b75eaa4f1a89b801695
Влад Любашенко <whatislaw007@outlook.com>
2024-01-14 15:45:07 +0200

    adapt pattern

---
+++ src/components/common/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="header">
+        <div class="header__container">
+            <h1 class="header__title">Welcome to resume creator!</h1>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+.header {
+    background-color: var(--main);
+}
+.header__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+}
+.header__title {
+    font-size: 20px;
+    color: var(--second);
+}
+</style>
