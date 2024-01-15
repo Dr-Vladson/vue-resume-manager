fb6c9f8d62af15633be564d1e78d6bd8d2b43539
Влад Любашенко <whatislaw007@outlook.com>
2024-01-15 12:54:31 +0200

    add store

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+    resumesSimpleArray: [],
+    currentFullResume : {},
+    setResumesSimpleArray(resumesSimpleArray) {
+        this.resumesSimpleArray = resumesSimpleArray
+    },
+    addToResumesSimpleArray (fullResume){
+        const {id, name, position} = fullResume
+        this.resumesSimpleArray.push({id, name, position})
+    },
+    setCurrentFullResume(fullResume){
+        this.currentFullResume = currentFullResume
+    }
+})
