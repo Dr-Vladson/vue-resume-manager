2fbc7e0122f8a92007fa8b7239ec106b33efc315
Влад Любашенко <whatislaw007@outlook.com>
2024-01-15 16:11:23 +0200

    add resume creaton validators

---
+++ src/features/validators.js
@@ -0,0 +1,26 @@
+function validateMaxSize (value, maxSize){
+    if (value.length > maxSize) return false
+    return true
+}
+function validateGmail (value){
+    if (value.endsWith("@gmail.com") && value.length > 10) return true
+    return false
+}
+function validatePhone (value){
+    if (value.starts("+380") && value.length === 13) return true
+    return false
+}
+export function getIsResumeValid (name, position, email, phone, intro, skills){
+    if 
+    (
+        validatePhone(phone) && 
+        validateGmail(email) &&
+        validateMaxSize(name, 30) &&
+        validateMaxSize(position, 30) &&
+        validateMaxSize(intro, 500) &&
+        validateMaxSize(skills, 150)
+    ){
+        return true
+    }
+    return false
+}
